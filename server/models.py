949d9af2dc00df82ec192e865feafef617ecc99e
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-07-30 13:57:15 +0300

    Model Constraints

--- server/models.py
+++ server/models.py
@@ -7,8 +7,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    _password_hash = db.column(db.String)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.column(db.String, nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -16,9 +16,9 @@ class User(db.Model):
 class Workout(db.Model):
     __tablename__ = "workouts"
     id = db.column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    duration = db.Column(db.Integer)
-    date = db.Column(db.Date)
+    title = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Date, nullable=False)
 
 
 
