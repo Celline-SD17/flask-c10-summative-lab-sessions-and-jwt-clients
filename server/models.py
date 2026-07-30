3bfdf308866a15e5e28a05f7a88eb26442a4c92a
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-07-30 14:16:57 +0300

    Password Hashing and Authentication

--- server/models.py
+++ server/models.py
@@ -11,6 +11,20 @@ class User(db.Model):
     _password_hash = db.column(db.String, nullable=False)
     workouts = db.relationship("Workout", back_populates="user", cascade="all, delete-orphan")
 
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes cannot be viewed!")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+
     def __repr__(self):
         return f'<User {self.username}>'
 
@@ -22,6 +36,9 @@ class Workout(db.Model):
     date = db.Column(db.Date, nullable=False)
     user_id = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
     user = db.Relationship("User", back_populates="workouts")
+
+    def __repr__(self):
+        return f'<Workout {self.title} {self.duration} {self.date}'
 
 
 
